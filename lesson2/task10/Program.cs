d71e817a2f3ed020f165ca11a1d705f997708a4e
ITX-USER <Fizik7776@protonmail.com>
2023-05-01 17:18:35 +0300

    ДЗ - Урок 2. Базовые алгоритмы

---
+++ lesson2/task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int random = new Random().Next(100, 1000);
+Console.WriteLine("Рандомное число: " + random);
+
+Console.WriteLine("Вторая цифра " + TheSecondDigit(random));
+
+int TheSecondDigit(int x)
+{
+    int resalt = x / 10;
+    resalt = resalt % 10;
+    return resalt;
+}
---
+++ lesson2/task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите трехзначное число: ");
+
+int n = int.Parse(Console.ReadLine());
+
+
+if (n <= 999)
+{
+    if (n >= 100)
+    {
+        int resalt = n % 10;
+        Console.WriteLine("Третья цифра " + resalt);
+    }
+    else
+    {
+        Console.WriteLine("Не трехзначное число!");
+    }
+}
+else
+    {
+        Console.WriteLine("Не трехзначное число!");
+    }
+
