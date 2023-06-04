c3ee37ea8c03dba86dfc9964155f495fe54b8133
ITX-USER <Fizik7776@protonmail.com>
2023-06-04 12:08:47 +0300

    дз урок 3

---
+++ lesson3/task19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/* 14212 -> нет
+
+   12821 -> да
+
+   23432 -> да */
+
+
+Console.Write("Введите пятизначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+/* int TheSecondDigit(int x)
+{
+    int resalt = x / 10;
+    resalt = resalt % 10;
+    return resalt;
+} */
